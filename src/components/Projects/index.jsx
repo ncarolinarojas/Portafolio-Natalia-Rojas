858866032151eeedb3e32bd38ce26bf7647e8149
ncarolinarojas <nataliarojasco22@gmail.com>
2023-12-01 00:36:12 -0500

    cards

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -12,12 +12,20 @@ const Projects = () => {
             </div>
             <div>
                 <Card
-                project={"Shopi E-commerce"}
-                tools={["React.js", " ,TailwindCSS", " , useContext", " , API Fake Platzi"]}
-                photo={photoShopi}
-                urlpage={"https://shopi-e-commerce.netlify.app/"}
-                use={"Make shopping, add products at card, view products after make the checkout and when user finally do the checkout, view their order."}
-                urlRepo={"https://github.com/ncarolinarojas/Shop_Prueba"}
+                    project={"Shopi E-commerce"}
+                    tools={["React.js", " ,TailwindCSS", " , useContext", " , API Fake Platzi"]}
+                    photo={photoShopi}
+                    urlPage={"https://shopi-e-commerce.netlify.app/"}
+                    use={"Make shopping, add products at card, view products after make the checkout and when user finally do the checkout, view their order."}
+                    urlRepo={"https://github.com/ncarolinarojas/Shop_Prueba"}
+                />
+                <Card
+                    project={"Countries APP"}
+                    tools={["React.js", " ,PostgreSQL", " , Node.js", " , SASS", " , Sequelize"]}
+                    photo={photoShopi}
+                    urlPage={"https://github.com/ncarolinarojas/Project-Countries"}
+                    use={"View countries and organize activities created for the user and save in data base"}
+                    urlRepo={"https://github.com/ncarolinarojas/Project-Countries"}
                 />
             </div>
         </div>
